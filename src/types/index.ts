e53d4134acecec54765b9d60137aab5f44eb17f4
Alena Fedorova <aaaaaalenaaaaaaaa@yandex.ru>
2025-01-20 01:55:59 +0300

    first review

---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+export interface ICard {
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number;
+}
+
+export interface ICustomer {
+    id: string;
+    email: string;
+    address: string;
+    phone: string;
+    paymentMethod: string;
+}
+
+export interface ICardData {
+    card: ICard[];
+    preview: string | null;
+}
+
+export type TCardInfo = Pick<ICard, 'image' | 'category' | 'title' | 'description' | 'price'>;
+
+export type TBasket = Pick<ICard, 'title' | 'price'>;
+
+export type TPaymentMethodAndAddress = Pick<ICustomer, 'paymentMethod' | 'address'>;
+
+export type TEmailAndPhone = Pick<ICustomer, 'email' | 'phone'>;
